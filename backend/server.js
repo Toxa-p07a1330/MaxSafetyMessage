a0492be0f3d45951bedb571dee92e47515088798
Toxa-p07a1330 <p07a1330@yandex.ru>
2023-02-07 16:53:14 +0300

    primarly connected to databaer

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('../database/messages.db');
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.get('/all', (req, res) => {
+    db.each("SELECT * FROM messages", (err, row) => {
+        console.log(row);
+        console.log(err)
+    });
+    res.send('Hello World!')
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
